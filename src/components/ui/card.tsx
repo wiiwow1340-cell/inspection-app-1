142b26683e0fdbd380a69b39e3e7c6ee23d450a3
wiiwow1340-cell <wiiwow1340@gmail.com>
2025-12-02 15:05:29 +0800

    Delete src directory

--- src/components/ui/card.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export function Card({children,className="",...props}) {
-  return <div className={"border rounded p-4 bg-white "+className} {...props}>{children}</div>;
-}
